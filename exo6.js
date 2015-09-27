e042b4755f865a5cd6efad25e155be79329e9203
STEEVEN LY <sl@smartapps.fr>
2015-09-27 15:28:13 +0200

    Javascript done

---
+++ exo6.js
@@ -0,0 +1,98 @@
+var rental = {
+  "cars": [
+    {
+      "id": "p306",
+      "vehicule": "peugeot 306",
+      "pricePerDay": 20,
+      "pricePerKm": 0.10
+    },
+    {
+      "id": "rr-sport",
+      "pricePerDay": 60,
+      "pricePerKm": 0.30
+    },
+    {
+      "id": "p-boxster",
+      "pricePerDay": 100,
+      "pricePerKm": 0.45
+    }
+  ],
+  "rentals": [
+    {
+      "id": "1-pb-92",
+      "driver": {
+        "firstName": "Paul",
+        "lastName": "Bismuth"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-12",
+      "distance": 100,
+      "options":{
+        "deductibleReduction": false
+      }
+    },
+    {
+      "id": "2-rs-92",
+      "driver": {
+        "firstName": "Rebecca",
+        "lastName": "Solanas"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-09-10",
+      "returnDate": "2015-09-15",
+      "distance": 300,
+      "options":{
+        "deductibleReduction": true
+      }
+    },
+    {
+      "id": "3-sa-92",
+      "driver": {
+        "firstName": " Sami",
+        "lastName": "Ameziane"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-08-31",
+      "returnDate": "2015-09-13",
+      "distance": 1000,
+      "options":{
+        "deductibleReduction": true
+      }
+    }
+  ],
+  "rentalModifications": [
+    {
+      "id": 1,
+      "rentalId": "1-pb-92" ,
+      "returnDate": "2015-09-13",
+      "distance": 150
+    },
+    {
+      "id": 2,
+      "rentalId": "3-sa-92",
+      "pickupDate": "2015-09-01"
+    }
+  ]
+}
+
+for (var i = 0; i < rental.cars.length; i++)
+{
+  for (var j = 0; j < rental.rentals.length; j++)
+  {
+    if (rental.rentals[j].car_id === rental.cars[i].id)
+    {
+      //Exo6
+
+      var distance, pickupDate, returnDate;
+
+      for (var k = 0; k < rental.rentalModifications.lenght; k++)
+      {
+        if (rental.rentalModifications[k].rentalId == rental.rentals[j].id)
+        {
+            //JSON mal formé
+        }
+      }
+    }
+  }
+}
